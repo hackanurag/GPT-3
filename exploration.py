0d3fceddd209083f075be2460d40efbceea8f528
hackanurag <anurag.datta.a@gmail.com>
2024-06-24 00:53:02 +0530

    removed older files

--- exploration.py
+++
@@ -1,7 +0,0 @@
-def explore():
-    import pandas as pd
-    df = pd.read_csv("hf://datasets/Kaludi/Customer-Support-Responses/Customer-Support.csv")
-    # print(df.head())s
-    return df
-
-# explore()
