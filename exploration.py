959bed6507ce1d695256b620ee31a330b6f454a1
hackanurag <anurag.datta.a@gmail.com>
2024-06-23 17:45:30 +0530

    Explored and preprocessed the dataset

---
+++ exploration.py
@@ -0,0 +1,7 @@
+def explore():
+    import pandas as pd
+    df = pd.read_csv("hf://datasets/Kaludi/Customer-Support-Responses/Customer-Support.csv")
+    # print(df.head())s
+    return df
+
+# explore()
